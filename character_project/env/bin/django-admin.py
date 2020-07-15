0cd64ba40a0562138e6aff9ab871d2022fec8811
rondeaumatias <mrondeau@ignetworks.com>
2020-07-15 08:42:48 -0300

    First commit, create django micro-service with docker-compose and the needed files to run it.

---
+++ character_project/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mrondeau/rondeaumatias/workspace/django/_coderio_exercise/character_project/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ config/gunicorn/conf.py
@@ -0,0 +1,5 @@
+name = 'character_project'
+loglevel = 'info'
+errorlog = '-'
+accesslog = '-'
+workers = 2
