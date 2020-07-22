ae02d0fc9cb4deb708bce40d8d322891cca13f00
rondeaumatias <mrondeau@ignetworks.com>
2020-07-22 18:55:42 -0300

    removing enviroment

--- character_project/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mrondeau/rondeaumatias/workspace/django/_coderio_exercise/character_project/env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
